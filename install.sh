8d68c66d61b8d3241e0793f190f2b05a3f5d4682
Trevor Coleman <trevor@trevorcoleman.design>
2020-10-16 12:19:02 -0400

    use variable for home folder

--- install.sh
+++ install.sh
@@ -1,26 +1,29 @@
 ECHO Removing existing files.
-rm ~/.config/starship.toml
-rm ~/.zshrc
-rm ~/.bash_profile
+rm $HOME/.config/starship.toml
+rm $HOME/.zshrc
+rm $HOME/.bash_profile
 ECHO installing starship
 curl -fsSL https://starship.rs/install.sh | bash
 ECHO installing Sublime Text
 brew cask install sublime-text 
-ln Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln Preferences.sublime-settings "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+mkdir $HOME/bin
+PATH=$PATH:$HOME/bin
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $HOME/bin/subl
 Echo installing iterm2
 brew cask install iterm2
-ln com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+ln com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
 ECHO installing fira code
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 brew cask install font-fira-code-nerd-font
 
 ECHO Creating Symlinks.
-ln .zshrc ~/.zshrc
-ln .bash_profile ~/.bash_profile
-ln .wave ~/.wave
-mkdir ~/.config
-ln starship.toml ~/.config/starship.toml
+ln .zshrc $HOME/.zshrc
+ln .bash_profile $HOME/.bash_profile
+ln .wave $HOME/.wave
+mkdir $HOME/.config
+ln starship.toml $HOME/.config/starship.toml
 
 cat .wave
 ECHO
